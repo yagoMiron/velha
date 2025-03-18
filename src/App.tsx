a6d630b1db2fca682651b9768ef0cd09fc46d139
Yago Verzas Miron <105978576+yagoMiron@users.noreply.github.com>
2025-03-17 22:37:26 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import JogoDaVelha from "./components/JogoDaVelha";
+import "./App.css";
 
 function App() {
   return (
-    <>
+    <main>
       <JogoDaVelha />
-    </>
+    </main>
   );
 }
 
